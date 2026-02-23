5ee773717c27c8cb776f759709c6a308c3fcfb26
isha-plexustechnology <isha.plexustechnology@gmail.com>
2026-02-23 16:40:30 +0530

    Update app.js file

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.use("/api", productRoutes);
 const PORT = process.env.PORT || 5006;
 
 app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 );
